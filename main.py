e5e4177e38c7c911d5e4dae46318cc2dbbb25bf6
Karan Yadure <karanyadure@gmail.com>
2026-08-19 13:04:33 +0530

    Add file upload functionality and UUID-based user directories

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask,render_template,request
 import uuid
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER ='user_uploads'
+ALLOWED_EXTENSIONS ={'png','jpg','jpeg'}
 
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
@@ -12,8 +18,20 @@ def create():
     myid=uuid.uuid1()
     if request.method == "POST":
         print(request.files.keys())
+        rec_id = request.form.get("uuid")
+        desc = request.form.get("text")
         for key, value in request.files.items():
             print(key, value)
+            file=request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if (not(os.path.exists(os.path.join(app.config["UPLOAD_FOLDER"],rec_id)))):
+                    os.mkdir(os.path.join(app.config["UPLOAD_FOLDER"],rec_id))
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"],rec_id, filename))
+
+            #Capture the  description and save it to a file
+            with open(os.path.join(app.config["UPLOAD_FOLDER"],rec_id,"desc.txt"),"w") as f:
+                f.write(desc)
 
     return render_template("create.html",myid=myid)
 
